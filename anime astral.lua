f03a1057615581a4fb17654a8b668bf0b33bd80a
knalha777 <yagovaz133@gmail.com>
2026-07-18 14:42:03 -0300

    Update anime astral.lua

--- anime astral.lua
+++ anime astral.lua
@@ -7,7 +7,7 @@ if not game:IsLoaded() then
 end
 
 -- CONFIGURAÇÕES
-local BASE_URL = "https://raw.githubusercontent.com/SEU_USUARIO/SEU_REPO/refs/heads/main"
+local BASE_URL = "https://raw.githubusercontent.com/knalha777/anime-astral-loader/refs/heads/main/anime%20astral.lua"
 local SCRIPT_NAME = "animeastral.lua"
 
 -- VERIFICA SE É O JOGO CORRETO
