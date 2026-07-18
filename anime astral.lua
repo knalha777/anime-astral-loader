f4a154b85f6c860dabcda10c933c46ca20b8a00b
knalha777 <yagovaz133@gmail.com>
2026-07-18 20:42:57 -0300

    Update anime astral.lua

--- anime astral.lua
+++ anime astral.lua
@@ -2,7 +2,7 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-local BASE_URL = "https://raw.githubusercontent.com/ZhangJunZ84/twvzyyds/refs/heads/main"
+local BASE_URL = "https://raw.githubusercontent.com/knalha777/anime-astral-loader/refs/heads/main"
 
 local GAMES = {
     [9186719164]  = "sailorpiece.lua",
